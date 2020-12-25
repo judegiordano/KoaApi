0f91775f16f2c3c5eca6abb38930a51b8c42b0d1
JudeBoy <judegiordano@gmail.com>
2020-12-25 18:41:29 -0500

    Remove UV_THREADPOOL_SIZE For Now

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -5,35 +5,34 @@ import { Environment } from "../types/Constants";
 dotenv.config();
 
 let env;
-const cpus = os.cpus().length;
-
-if (process.env.NODE_ENV === Environment.stg) {
-	env = Environment.stg;
-	process.env.UV_THREADPOOL_SIZE = (cpus / 2).toString();
-}
-else if (process.env.NODE_ENV === Environment.prod) {
+let cors;
+if (process.env.NODE_ENV === Environment.prod) {
 	env = Environment.prod;
-	process.env.UV_THREADPOOL_SIZE = cpus.toString();
+	cors = os.cpus().length;
+}
+else if (process.env.NODE_ENV === Environment.stg) {
+	env = Environment.stg;
+	cors = Math.ceil(os.cpus().length / 2);
 }
 else {
 	env = Environment.dev;
-	process.env.UV_THREADPOOL_SIZE = "1";
+	cors = 1;
 }
 
 const config = {
-	PORT: parseInt(process.env.PORT) || 3000,
-	DB_TYPE: process.env.DB_TYPE || undefined,
-	DB_HOST: process.env.DB_HOST || undefined,
-	DB_PORT: parseInt(process.env.DB_PORT) || 1433,
-	DB_USERNAME: process.env.DB_USERNAME || undefined,
-	DB_PASSWORD: process.env.DB_PASSWORD || undefined,
-	DB_NAME: process.env.DB_NAME || undefined,
-	JWT_SECRET: process.env.JWT_SECRET || undefined,
-	NODE_ENV: env,
-	CORES: <number>parseInt(process.env.UV_THREADPOOL_SIZE),
-	IS_COMPILED: path.extname(__filename).includes("js"),
-	LIMIT: 30,
-	LIMIT_RESET: (60000 * 10)  // 10 minutes
+	PORT: <number>parseInt(process.env.PORT) || 3000,
+	DB_TYPE: <string>process.env.DB_TYPE || undefined,
+	DB_HOST: <string>process.env.DB_HOST || undefined,
+	DB_PORT: <number>parseInt(process.env.DB_PORT) || 1433,
+	DB_USERNAME: <string>process.env.DB_USERNAME || undefined,
+	DB_PASSWORD: <string>process.env.DB_PASSWORD || undefined,
+	DB_NAME: <string>process.env.DB_NAME || undefined,
+	JWT_SECRET: <string>process.env.JWT_SECRET || undefined,
+	NODE_ENV: <Environment>env,
+	CORES: <number>cors,
+	IS_COMPILED: <boolean>path.extname(__filename).includes("js"),
+	LIMIT: <number>30,
+	LIMIT_RESET: <number>(60000 * 10)  // 10 minutes
 };
 
 if (config.DB_TYPE === undefined) {

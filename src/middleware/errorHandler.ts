6e88e19a72b6517d5f50c34ee86d1381ad8c0d02
JudeBoy <judegiordano@gmail.com>
2020-12-27 17:06:37 -0500

    Remove Log in ErrorHandler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,12 +1,10 @@
 import Koa from "koa";
 import { IErr } from "../types/IRoute";
-import log from "../services/logger";
 
 export default async (ctx: Koa.Context, next: Koa.Next) => {
 	try {
 		await next();
 	} catch (e) {
-		log.error(e.message);
 		ctx.status = 500;
 		ctx.body = {
 			ok: false,

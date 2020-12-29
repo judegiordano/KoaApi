7b6974c1bf028d6d6f7e440124771616a654f534
JudeBoy <judegiordano@gmail.com>
2020-12-29 16:11:57 -0500

    Modify Loadtester

--- local.dev/locustfile.py
+++ local.dev/locustfile.py
@@ -1,7 +1,22 @@
 import time
-from locust import HttpUser, task
+import json
+from locust import HttpUser, TaskSet, task
 
-class QuickstartUser(HttpUser):
-    @task
+class UserBehaviour(TaskSet):
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        self.client.post("/api/user/login", {'email': 'newmail@mail.com', 'password': 'Test1234!'})
+
+    @task(1)
+    def post_login(self):
+        self.login()
+
+    @task(1)
     def get_by_id(self):
         self.client.get(url="/api/user/id/5")
+
+class User(HttpUser):
+    tasks=[UserBehaviour]
+    host='https://localhost:3000'
